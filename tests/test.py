5cecd6625ca25763e32fff6cc6203166f0d85e18
Raphaël Vinot <raphael@vinot.info>
2015-10-27 10:50:46 +0100

    Add more tests

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,8 @@
 import unittest
 
 from bin.specific import KittenGroomerSpec
+from bin.pier9 import KittenGroomerPier9
+from bin.generic import KittenGroomer
 
 
 class TestBasic(unittest.TestCase):
@@ -13,4 +15,12 @@ class TestBasic(unittest.TestCase):
 
     def test_specific(self):
         spec = KittenGroomerSpec('tests/src', 'tests/dst')
+        spec.processdir()
+
+    def test_pier9(self):
+        spec = KittenGroomerPier9('tests/src', 'tests/dst')
+        spec.processdir()
+
+    def test_generic(self):
+        spec = KittenGroomer('tests/src', 'tests/dst')
         spec.processdir()
