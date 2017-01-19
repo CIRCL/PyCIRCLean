3041c6303fb271ce60b13b90babb961f4c5ab663
Dan Puttick <dputtick@users.noreply.github.com>
2017-01-19 17:48:14 -0500

    Update changelog to version 2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='kittengroomer',
-    version='2.0.2',
+    version='2.1',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
