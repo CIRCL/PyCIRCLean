d470d6bb21aeea0ce86cd435210458de6337dac6
Dan Puttick <dputtick@users.noreply.github.com>
2017-04-12 15:50:02 -0400

    Open test log in bytes mode

--- tests/logging.py
+++ tests/logging.py
@@ -5,21 +5,24 @@ from datetime import datetime
 def save_logs(groomer, test_description):
     divider = ('=' * 10 + '{}' + '=' * 10 + '\n')
     test_log_path = 'tests/test_logs/{}.log'.format(test_description)
-    with open(test_log_path, 'w+') as test_log:
-        test_log.write(divider.format('TEST LOG'))
-        test_log.write(str(datetime.now().time()) + '\n')
-        test_log.write(test_description + '\n')
-        test_log.write('-' * 20 + '\n')
+    time_now = str(datetime.now().time()) + '\n'
+    with open(test_log_path, 'wb+') as test_log:
+        log_header = divider.format('TEST LOG')
+        test_log.write(bytes(log_header, encoding='utf-8'))
+        test_log.write(bytes(time_now, encoding='utf-8'))
+        test_log.write(bytes(test_description, encoding='utf-8'))
+        test_log.write(b'\n')
+        test_log.write(b'-' * 20 + b'\n')
         with open(groomer.logger.log_path, 'rb') as logfile:
             log = logfile.read()
             test_log.write(log)
         if os.path.exists(groomer.logger.log_debug_err):
-            test_log.write(divider.format('ERR LOG'))
+            test_log.write(bytes(divider.format('ERR LOG'), encoding='utf-8'))
             with open(groomer.logger.log_debug_err, 'rb') as debug_err:
                 err = debug_err.read()
                 test_log.write(err)
         if os.path.exists(groomer.logger.log_debug_out):
-            test_log.write(divider.format('OUT LOG'))
+            test_log.write(bytes(divider.format('OUT LOG'), encoding='utf-8'))
             with open(groomer.logger.log_debug_out, 'rb') as debug_out:
                 out = debug_out.read()
                 test_log.write(out)
